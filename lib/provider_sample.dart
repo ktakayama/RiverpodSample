3bd9b8022358e9eef6c1197e166037c892925c20
Kyosuke Takayama <loiseau@gmail.com>
2022-06-05 08:59:41 +0900

    add: new provider

--- lib/provider_sample.dart
+++ lib/provider_sample.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sampleProvider = Provider((_) => 999);
-final randomProvider = Provider((_) => Random().nextInt(100));
 final dsiposeProvider = Provider.autoDispose((_) => Random().nextInt(100));
+final randomProvider = Provider((_) => Random().nextInt(100));
+final randomDoubleProvider = Provider((ref) => ref.watch(randomProvider) * 2);
 
 class ProviderSample extends ConsumerWidget {
   const ProviderSample({super.key});
@@ -13,14 +14,15 @@ class ProviderSample extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final sampleValue = ref.watch(sampleProvider);
-    final randomValue = ref.watch(randomProvider);
     final disposeValue = ref.watch(dsiposeProvider);
+    final randomValue = ref.watch(randomProvider);
+    final randomDoubleValue = ref.watch(randomDoubleProvider);
     return Scaffold(
         appBar: AppBar(title: const Text('FutureProviderSample')),
         body: Center(
             child: Text(
-                'sampleValue: $sampleValue\nrandomValue: $randomValue\ndisposeValue: $disposeValue',
-                textScaleFactor: 2)),
+                'sampleValue: $sampleValue\ndisposeValue: $disposeValue\nrandomValue: $randomValue\nrandomDouble: $randomDoubleValue',
+                textScaleFactor: 1.8)),
         floatingActionButton: FloatingActionButton(
           child: const Icon(Icons.refresh),
           onPressed: () => ref
