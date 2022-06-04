3864eb7abb86e056979c0edf562bc67795a6d361
Kyosuke Takayama <loiseau@gmail.com>
2022-06-05 08:24:04 +0900

    add: autoDispose sample

--- lib/provider_sample.dart
+++ lib/provider_sample.dart
@@ -4,8 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final sampleProvider = Provider((_) => 999);
-
 final randomProvider = Provider((_) => Random().nextInt(100));
+final dsiposeProvider = Provider.autoDispose((_) => Random().nextInt(100));
 
 class ProviderSample extends ConsumerWidget {
   const ProviderSample({super.key});
@@ -14,10 +14,12 @@ class ProviderSample extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final sampleValue = ref.watch(sampleProvider);
     final randomValue = ref.watch(randomProvider);
+    final disposeValue = ref.watch(dsiposeProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('FutureProviderSample')),
       body: Center(
-          child: Text('sampleValue: $sampleValue\nrandomValue: $randomValue',
+          child: Text(
+              'sampleValue: $sampleValue\nrandomValue: $randomValue\ndisposeValue: $disposeValue',
               textScaleFactor: 2)),
     );
   }
